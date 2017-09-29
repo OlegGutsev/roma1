d8c6126b098763cfc918cc54f3c5c563d42111bc
Oleg Gutsev <olezhka.fanky@mail.ru>
2017-09-29 14:01:47 +0300

    123

---
+++ roma1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roma1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
