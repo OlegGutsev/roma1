29b09c6864a1630d009ba01eef07ace1633e3666
Oleg Gutsev <olezhka.fanky@mail.ru>
2017-09-29 18:03:48 +0300

    1321

--- roma1/Program.cs
+++ roma1/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace roma1
 {
+
+    class SuperList<T> : List<T> where T : class
+    {
+     
+        public SuperList()
+        {
+  
+        }
+         
+        public static SuperList<T> operator +(SuperList<T> a, T b)
+        {
+            SuperList<T> m;
+               if (a.Count >= 3)
+                throw new IndexOutOfRangeException();
+
+          
+             a.Add(b);
+            m = a;
+            return m;
+
+        }
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            string a = "Oleg";
+            SuperList<string> obj = new SuperList<string>();
+            SuperList<string> obj1 = new SuperList<string>();
+            try
+            {
+                for (int i = 0; i <4; i++)
+                {
+                    obj = obj + a;
+                }
+                
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            foreach (var item in obj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }
